32b251e7acb7f158cb4a02835b8d4a111f01745f
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-05 22:11:38 +0700

    Init

---
+++ src/main/java/com/example/steamreplica/SteamReplicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.steamreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamReplicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamReplicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/steamreplica/SteamReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.steamreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamReplicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
