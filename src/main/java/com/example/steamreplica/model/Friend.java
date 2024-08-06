eb45cc3f59923f42ab961f5eee2215ce37a8d6ed
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-06 13:38:17 +0700

    Add user friend

---
+++ src/main/java/com/example/steamreplica/model/Friend.java
@@ -0,0 +1,29 @@
+package com.example.steamreplica.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Friend {
+    @Id
+    @GeneratedValue
+    private long id;
+    
+    private ZonedDateTime friendSince;
+    
+    @ManyToOne
+    @JoinColumn(name = "friend_Id", referencedColumnName = "id")
+    private User friend;
+
+    @ManyToOne
+    @JoinColumn(name = "user_Id", referencedColumnName = "id")
+    private User user;
+}
--- src/main/java/com/example/steamreplica/model/User.java
+++ src/main/java/com/example/steamreplica/model/User.java
@@ -16,24 +16,24 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    
+
     @Id
     @GeneratedValue
     private long id;
-    
+
     @NotBlank(message = "User name can not be empty")
     private String userName;
-    
+
     @Lob
     @Size(max = 1048576 * 5, message = "Profile picture must be less than 5MB")
     private Blob userProfilePicture;
-    
+
     private String phoneNumber;
-    
+
     @Email
     @NotBlank(message = "Email can not be empty")
     private String email;
-    
+
     private UserStatus Status;
 
     @Size(min = 12, max = 50, message = "Password must be between 12 and 50 characters")
@@ -43,4 +43,12 @@ public class User {
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "UserRole", joinColumns = @JoinColumn(name = "user_Id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_Id", referencedColumnName = "id"))
     private Set<ApplicationRole> roles = new HashSet<>();
+
+    // Friends of this user
+    @OneToMany(mappedBy = "user")
+    private Set<Friend> friends = new HashSet<>();
+
+    // This user is friend to ...
+    @OneToMany(mappedBy = "friend")
+    private Set<Friend> friendOf = new HashSet<>();
 }
