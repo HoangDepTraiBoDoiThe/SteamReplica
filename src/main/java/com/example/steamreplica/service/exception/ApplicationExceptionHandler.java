4ba376fa06be024eb8947706bbcc5b613673d97b
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-11 14:20:40 +0700

    error handler

--- src/main/java/com/example/steamreplica/service/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/example/steamreplica/service/exception/ApplicationExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,23 +24,39 @@ class ErrorDataType {
 public class ApplicationExceptionHandler {
     private final String serverExceptionMessage = "Server exception: %s";
     private final String  causer = "Causer message error: %s";
-    private final String  internalMessage = "Internal error: %s";
 
     @ExceptionHandler(GameException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<?> handleGameNotFoundException(Exception ex, WebRequest webRequest) {
-        String errorMessage = String.format(serverExceptionMessage, ex.getMessage());
-        String causerMessage = ex.getCause().getCause().getMessage();
-        ErrorDataType exceptionData = new ErrorDataType(String.format(internalMessage, errorMessage), String.format(causer, causerMessage), webRequest.getDescription(false));
+    public ResponseEntity<?> handleGameNotFoundException(GameException ex, WebRequest webRequest) {
+        ErrorDataType exceptionData = extractErrorData(ex, webRequest);
+        return new ResponseEntity<>(exceptionData, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<?> handleUserNotFoundException(UserException ex, WebRequest webRequest) {
+        ErrorDataType exceptionData = extractErrorData(ex, webRequest);
+
+        return new ResponseEntity<>(exceptionData, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<?> handleUserNotFoundException(UsernameNotFoundException ex, WebRequest webRequest) {
+        ErrorDataType exceptionData = extractErrorData(ex, webRequest);
         return new ResponseEntity<>(exceptionData, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     ResponseEntity<?> handleOtherException(Exception ex, WebRequest webRequest) {
-        String errorMessage = ex.getMessage();
-        String causerMessage = ex.getCause().getCause().getMessage();
-        ErrorDataType exceptionData = new ErrorDataType(String.format(internalMessage, errorMessage), String.format(causer, causerMessage), webRequest.getDescription(false));
+        ErrorDataType exceptionData = extractErrorData(ex, webRequest);
         return new ResponseEntity<>(exceptionData, HttpStatus.NOT_FOUND);
+    }
+
+    ErrorDataType extractErrorData(Exception ex, WebRequest webRequest) {
+        String errorMessage = String.format(serverExceptionMessage, ex.getMessage());
+        String causerMessage = String.format(serverExceptionMessage, ex.getMessage());
+        if (ex.getCause() != null && ex.getCause().getCause() != null) causerMessage = ex.getCause().getCause().getMessage();
+        return new ErrorDataType(errorMessage, String.format(causer, causerMessage), webRequest.getDescription(false));
     }
 }
