ccb44e9a205ed6458783392cc0c36ae1f5874cb1
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-14 22:06:54 +0700

    feat(security): add MyPermissionEvaluator

---
+++ src/main/java/com/example/steamreplica/util/MyPermissionEvaluator.java
@@ -0,0 +1,21 @@
+package com.example.steamreplica.util;
+
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class MyPermissionEvaluator implements PermissionEvaluator {
+    @Override
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        return false;
+        
+    }
+}
