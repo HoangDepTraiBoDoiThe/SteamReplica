c880c56c69c6ea673f83635baee5ab47cf2c49be
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-31 14:48:04 +0700

    chore(config): remove unnecessary redis configuration

--- src/main/java/com/example/steamreplica/config/RedisConfig.java
+++ src/main/java/com/example/steamreplica/config/RedisConfig.java
@@ -2,15 +2,10 @@ package com.example.steamreplica.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -30,16 +25,16 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    @Bean
-    public RedisSerializer<Object> redisSerializer() {
-        return new GenericJackson2JsonRedisSerializer();
-    }
-
-    @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(60))
-                .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer()));
-    }
+//    @Bean
+//    public RedisSerializer<Object> redisSerializer() {
+//        return new GenericJackson2JsonRedisSerializer();
+//    }
+//
+//    @Bean
+//    public RedisCacheConfiguration cacheConfiguration() {
+//        return RedisCacheConfiguration.defaultCacheConfig()
+//                .entryTtl(Duration.ofMinutes(60))
+//                .disableCachingNullValues()
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer()));
+//    }
 }
